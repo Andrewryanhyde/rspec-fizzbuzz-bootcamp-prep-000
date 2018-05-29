7fd8dc79d914c90e7024aac097b3e19a57c547bf
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-05-29 08:21:12 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,12 @@
 def fizzbuzz(number)
   
   if number % 3 == 0
-    puts "Fizz" end
+    puts "Fizz" 
+  end
   
   else if number % 5 == 0 
     puts "Buzz"
-    end
+  end
   
   else if number % 3 == 0 && number % 5 == 0 
     puts "FizzBuzz"
