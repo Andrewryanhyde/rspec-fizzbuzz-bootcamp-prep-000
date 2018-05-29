adc9f1a69cd9b86546fad013687a0f9d849e92da
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-05-29 08:30:03 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,13 +1,13 @@
 def fizzbuzz(number)
   
   if number % 3 == 0
-    puts "Fizz" 
+    return "Fizz" 
     
   elseif number % 5 == 0 
-    puts "Buzz"
+    return "Buzz"
     
   elseif number % 3 == 0 && number % 5 == 0 
-    puts "FizzBuzz"
+    return "FizzBuzz"
     
   else
     puts "The number you entered is not divisible by three or five" 
